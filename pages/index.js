3aa55a56511d37b2b1c99f2d9a4e8431f92b18ab
Heidiki <abdelattie@gmail.com>
2022-07-29 08:25:13 +0100

    get the homes and list em'

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import { PrismaClient } from '@prisma/client'
 
-import homes from 'data.json';
+const prisma = new PrismaClient()
 
-export default function Home() {
+export default function Home({ homes = [] }) {
   return (
     <Layout>
       <h1 className="text-xl font-medium text-gray-800">
@@ -17,4 +18,15 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const homes = await prisma.home.findMany();
+
+  return {
+    props: {
+      homes: JSON.parse(JSON.stringify(homes)),
+      // props for the Home component
+    },
+  };
 }
